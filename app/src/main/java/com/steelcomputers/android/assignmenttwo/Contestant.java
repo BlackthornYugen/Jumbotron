3a7924098393b6f8a0635db43fa37dc59f24066b
Manuel Lopez <naaturaz@gmail.com>
2015-12-07 15:19:58 -0500

    Contestant and Point Classes Added

---
+++ app/src/main/java/com/steelcomputers/android/assignmenttwo/Contestant.java
@@ -0,0 +1,10 @@
+package com.steelcomputers.android.assignmenttwo;
+
+/**
+ * Created by Manuel on 12/7/2015.
+ */
+public class Contestant {
+
+    boolean isATeam;
+
+ }
---
+++ app/src/main/java/com/steelcomputers/android/assignmenttwo/Point.java
@@ -0,0 +1,7 @@
+package com.steelcomputers.android.assignmenttwo;
+
+/**
+ * Created by Manuel on 12/7/2015.
+ */
+public class Point {
+}
