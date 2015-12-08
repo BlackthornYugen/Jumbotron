1738d3304bd44b707e6d832184993ed124c98b41
John Steel <john@steelcomputers.com>
2015-12-08 10:32:19 -0500

    Contestant & Point extend ParseObject added a getter/setter

--- app/src/main/java/com/steelcomputers/android/assignmenttwo/Contestant.java
+++ app/src/main/java/com/steelcomputers/android/assignmenttwo/Contestant.java
@@ -1,9 +1,11 @@
 package com.steelcomputers.android.assignmenttwo;
 
+import com.parse.ParseObject;
+
 /**
  * Created by Manuel on 12/7/2015.
  */
-public class Contestant {
+public class Contestant extends ParseObject{
 
     boolean isATeam;//will indicate if is a team the contestant
 
--- app/src/main/java/com/steelcomputers/android/assignmenttwo/Point.java
+++ app/src/main/java/com/steelcomputers/android/assignmenttwo/Point.java
@@ -1,15 +1,24 @@
 package com.steelcomputers.android.assignmenttwo;
 
+import com.parse.ParseObject;
+
 import java.sql.Time;
 
 /**
  * Created by Manuel on 12/7/2015.
  */
-public class Point
+public class Point extends ParseObject
 {
     Game currentGame;
     Contestant contestant;
     Time timeStamp;
 
+    public static class COLUMN {
+        public static final String GAME = "game";
+        public static final String CONTESTANT  = "contestant";
+        public static final String TIME = "time";
+    }
 
+    public Contestant getContestant() {return (Contestant)getParseObject(COLUMN.CONTESTANT);};
+    public void setContestant(Contestant contestant) {put(COLUMN.CONTESTANT, contestant);};
 }
